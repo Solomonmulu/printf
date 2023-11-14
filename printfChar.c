98eccddafca166c6d1daa845c57122f406c540b2
AntipasG <antipasbelihu@gmail.com>
2023-11-14 20:42:07 +0300

    printf checking for error on betty

--- printfChar.c
+++ printfChar.c
@@ -79,39 +79,34 @@ int _strlenc(const char *str)
  */
 int printfExclusiveString(va_list args)
 {
-    char *str;
-    int i = 0, ln = 0;
-    int cast;
+	char *str;
+	int i = 0, ln = 0;
+	int cast;
 
-    str = va_arg(args, char *);
-    if (str == NULL)
-        str = "(null)";
-
-    while (str[i] != '\0')
-    {
-        if (str[i] < 32 || str[i] >= 127)
-        {
-            _putchar('\\');
-            _putchar('x');
-            ln += 2;
-
-            cast = str[i];
-            if (cast < 16)
-            {
-                _putchar('0');
-                ln++;
-            }
-
-            ln += printfHEXAux(cast);
-        }
-        else
-        {
-            _putchar(str[i]);
-            ln++;
-        }
-
-        i++;
-    }
-
-    return ln;
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			ln += 2;
+			cast = str[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				ln++;
+			}
+			ln += printfHEXAux(cast);
+		}
+		else
+		{
+			_putchar(str[i]);
+			ln++;
+		}
+		i++;
+	}
+	return (ln);
 }
