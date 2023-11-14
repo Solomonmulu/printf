b91bf213506bd4aa332ead13e3418447de8e1626
root <root@DESKTOP-BSA9J6K>
2023-11-14 19:51:15 +0300

    all

--- printfChar.c
+++ printfChar.c
@@ -73,40 +73,45 @@ int _strlenc(const char *str)
 }
 
 /**
- * printfExclusiveString - print exclusuives string.
- * @args: argumen t.
+ * printfExclusiveString - print exclusive string.
+ * @args: argument.
  * Return: the length of the string.
  */
-
 int printfExclusiveString(va_list args)
 {
-	char *str;
-	int i, ln = 0;
-	int cast;
+    char *str;
+    int i = 0, ln = 0;
+    int cast;
 
-	str = va_arg(args, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			ln = ln + 2;
-			cast = str[i];
-			if (cast < 16)
-			{
-				_putchar('0');
-				ln++;
-			}
-			ln = ln + printfHEXAux(cast);
-		}
-		else
-		{
-			_putchar(str[i]);
-			ln++;
-		}
-	}
-	return (ln);
+    str = va_arg(args, char *);
+    if (str == NULL)
+        str = "(null)";
+
+    while (str[i] != '\0')
+    {
+        if (str[i] < 32 || str[i] >= 127)
+        {
+            _putchar('\\');
+            _putchar('x');
+            ln += 2;
+
+            cast = str[i];
+            if (cast < 16)
+            {
+                _putchar('0');
+                ln++;
+            }
+
+            ln += printfHEXAux(cast);
+        }
+        else
+        {
+            _putchar(str[i]);
+            ln++;
+        }
+
+        i++;
+    }
+
+    return ln;
 }
