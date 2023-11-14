65343cf5d793553b3b56b4330d69d13f540f6679
root <root@DESKTOP-BSA9J6K>
2023-11-14 16:08:16 +0300

    all

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * printf_rot13 - printf str to ROT13 place into buffer
  * @args: type struct va_arg where is allocated printf arguments
@@ -35,26 +34,4 @@ int printf_rot13(va_list args)
 		}
 	}
 	return (counter);
-}
-
-/**
- * printf_srev - function that prints a str in reverse
- * @args: type struct va_arg where is allocated printf arguments
- *
- * Return: the string
- */
-int printf_srev(va_list args)
-{
-
-	char *s = va_arg(args, char*);
-	int i;
-	int j = 0;
-
-	if (s == NULL)
-		s = "(null)";
-	while (s[j] != '\0')
-		j++;
-	for (i = j - 1; i >= 0; i--)
-		_putchar(s[i]);
-	return (j);
 }
--- printf_str.c
+++ printf_str.c
@@ -13,7 +13,6 @@ int _strlen(char *s)
 	return (c);
 
 }
-
 /**
  * _strlenc - Strlen function but applied for constant char pointer s
  * @s: Type char pointer
@@ -26,34 +25,4 @@ int _strlenc(const char *s)
 	for (c = 0; s[c] != 0; c++)
 		;
 	return (c);
-}
-
-/**
- * printf_pointer - prints an hexgecimal number.
- * @val: arguments.
- * Return: counter.
- */
-int printf_pointer(va_list val)
-{
-	void *p;
-	char *s = "(nil)";
-	long int a;
-	int b;
-	int i;
-
-	p = va_arg(val, void*);
-	if (p == NULL)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
-		return (i);
-	}
-
-	a = (unsigned long int)p;
-	_putchar('0');
-	_putchar('x');
-	b = printf_hex_aux(a);
-	return (b + 2);
 }
