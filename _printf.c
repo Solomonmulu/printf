6c471add62ee92cfbece63b698b39b6bd0ba5213
Solomon mulu <114971695+Solomonmulu@users.noreply.github.com>
2023-11-14 22:14:22 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,45 +1,67 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * process_format_specifiers - Process the format string and call the appropriate functions.
+ * @format: The format string.
+ * @args: The variable arguments list.
+ * Return: The total length of the printed string.
+ */
+static int process_format_specifiers(const char *format, va_list args)
+{
+    int i = 0, j, len = 0;
+    convert_match m[] = {
+        {"%s", printfString}, {"%c", printfChar},
+        {"%%", printfpercent},
+        {"%i", printfInt}, {"%d", printfDec}, {"%r", printfRev},
+        {"%R", printfRot13}, {"%b", printfBin}, {"%u", printfUnsigned},
+        {"%o", printfOct}, {"%x", printfHex}, {"%X", printfHEX},
+        {"%S", printfExclusiveString}, {"%p", printfPointer}
+    };
+
+    while (format[i] != '\0')
+    {
+        j = 13;
+        while (j >= 0)
+        {
+            if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+            {
+                len += m[j].f(args);
+                i = i + 2;
+                break;
+            }
+            j--;
+        }
+        if (j < 0) // No match found for format specifier
+        {
+            _putchar(format[i]);
+            len++;
+            i++;
+        }
+    }
+
+    return len;
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * Return: The total length of the printed string.
  */
 int _printf(const char * const format, ...)
 {
-	int i = 0, j, len = 0;
-	va_list args;
-	convert_match m[] = {
-		{"%s", printfString}, {"%c", printfChar},
-		{"%%", printfpercent},
-		{"%i", printfInt}, {"%d", printfDec}, {"%r", printfRev},
-		{"%R", printfRot13}, {"%b", printfBin}, {"%u", printfUnsigned},
-		{"%o", printfOct}, {"%x", printfHex}, {"%X", printfHEX},
-		{"%S", printfExclusiveString}, {"%p", printfPointer}
-	};
+    int len;
+    va_list args;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+    {
+        va_end(args);
+        return -1;
+    }
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+    len = process_format_specifiers(format, args);
+
+    va_end(args);
+    return len;
 }
