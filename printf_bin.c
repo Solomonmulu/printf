c44847b12ea6ede1053866c10a70b1a0511fd6d0
root <root@DESKTOP-BSA9J6K>
2023-11-14 15:18:45 +0300

    all

---
+++ printf_bin.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * printf_bin - prints a binary number.
+ * @val: arguments.
+ * Return: 1.
+ */
+int printf_bin(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
+
+/**
+ * printf_oct - prints an octal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_oct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
