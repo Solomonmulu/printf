6213e15c1fc65f87ba01c8f38edb73b592cc70db
root <root@DESKTOP-BSA9J6K>
2023-11-14 16:25:57 +0300

    all

--- printf_char.c
+++ printf_char.c
@@ -13,3 +13,61 @@ int printfChar(va_list val)
 	_putchar(s);
 	return (1);
 }
+
+/**
+ * printfString - print a string.
+ * @val: argumen t.
+ * Return: the length of the string.
+ */
+
+int printfString(va_list val)
+{
+	char *s;
+	int i, len;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+}
+
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: c.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+
+/**
+ * _strlenc - Strlen function but applied for constant char pointer s
+ * @s: Type char pointer
+ * Return: c
+ */
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
